a62f0e553bc2ceec66b3ec218b63acdaa5fdb666
Noel Delgado <noeldelgadom@gmail.com>
2017-05-02 20:45:37 -0500

    modelos listos

--- app_one/models.py
+++ app_one/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Goat(models.Model):
+    name = models.CharField(max_length=128)
+    age = models.PositiveIntegerField()
+    description = models.TextField()
+    profile_pic = models.ImageField(upload_to="")
+    pastor = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
